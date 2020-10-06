2bf87549eb383c473e4bbb04b804b09798566e61
Alex <ignatenko2207@gmail.com>
2020-10-06 13:16:10 +0300

    Add page scrapper

--- src/main/groovy/io/techmap/scrape/scraper/webscraper/TotaljobsScraper.groovy
+++ src/main/groovy/io/techmap/scrape/scraper/webscraper/TotaljobsScraper.groovy
@@ -7,6 +7,9 @@ import groovy.util.logging.Log4j2
 import io.techmap.scrape.data.Company
 import io.techmap.scrape.data.Job
 import io.techmap.scrape.data.Location
+import io.techmap.scrape.data.shared.Address
+import io.techmap.scrape.data.shared.Contact
+import io.techmap.scrape.data.shared.GeoPoint
 import io.techmap.scrape.data.shared.TagType
 import io.techmap.scrape.helpers.DataCleaner
 import org.bson.Document
@@ -14,7 +17,6 @@ import org.jsoup.HttpStatusException
 import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
-import java.time.LocalDate
 import java.time.ZonedDateTime
 
 @Log4j2
@@ -73,8 +75,8 @@ class TotaljobsScraper extends AWebScraper {
         def paginationPage = loadPage(nextURL)
 
 
-        int offersAmount = paginationPage.select("div[class=job-title]").size()
-        int totalAmount = paginationPage.select("div[class=page-title]").first().text().replaceAll("\\D", "").toInteger()
+        int offersAmount = paginationPage.select("div.job-title").size()
+        int totalAmount = paginationPage.select("div.page-title").first().text().replaceAll("\\D", "").toInteger()
 
 		// we should include the first one without numeration parameter
 		int lastPageInGroup = ((totalAmount / offersAmount) + 1) as Integer
@@ -85,11 +87,13 @@ class TotaljobsScraper extends AWebScraper {
 		while (pageNumber <= lastPageInGroup) {
 			log.debug "... scrape list page ${pageNumber} with ${offersAmount} offers in group ${group.text()}"
 			// extract job offer links
-			def jobOffers = paginationPage.select("div[class=job-title] a")
+			def jobOffers = paginationPage.select("div.job-title a")
 			// check jobs and break loop if missed - total amount can be higher than expected
+			// f.e I've got more 4000 offers in downloaded document,
+			// but actually(after script) there were less than 2000
 			if (jobOffers) {
 				for (Element element : jobOffers) {
-					if (scrapePage(element.absUrl("href"), [category: group.text()])) {
+					if (scrapePage(element?.absUrl("href"), [category: group.text()])) {
 						jobsInGroupCount++
 					}
 				}
@@ -121,111 +125,142 @@ class TotaljobsScraper extends AWebScraper {
 			def jobPage = loadPage(pageURL)
 			if (!jobPage) return false
 
-			/*******************************/
-			/* Extract data in JSON format */
-			/*******************************/
-
-			final JsonSlurper jsonSlurper = new JsonSlurper()	// thread safe and serializable - alternative: new HashMap<>(jsonSlurper.parseText(jsonText))
-			def dataRaw		= jobPage?.select("script")*.html().find { it.contains("var utag_data") }?.replaceAll(/(?s).*var utag_data = ([^;]+);/, '$1')?.replaceAll(/(?s)\s+/, ' ')
-			def data		= jsonSlurper.parseText(dataRaw ?: "{}")
-			def dataRaw2	= jobPage?.select("script")*.html().find { it.contains("LocationOnlyMapBlock") }?.replaceAll(/(?s).*LocationOnlyMapBlock = ([^;]+);/, '$1')?.replaceAll(/(?s)\s+/, ' ')
-			def data2		= jsonSlurper.parseText(dataRaw2 ?: "{}")
-			def dataRaw2b	= jobPage?.select("script")*.html().find { it.contains("LocationWithCommuteTimeBlock") }?.replaceAll(/(?s).*LocationWithCommuteTimeBlock = ([^;]+);/, '$1')?.replaceAll(/(?s)\s+/, ' ')
-			data2 = data2 ?: jsonSlurper.parseText(dataRaw2b ?: "{}")
-			def data3
-			try { data3 = jsonSlurper.parseText(jobPage?.select(".js-sticky-bar")*.attr(".data-apply-button") ?: "{}") } catch (e) { /*ignore*/ }
-			def dataRaw4	= jobPage?.select("script#js-section-preloaded-HeaderStepStoneBlock")?.first()?.html()?.replaceAll(/(?s).*PRELOADED_STATE__.HeaderStepStoneBlock = ([^;]+);/, '$1')?.replaceAll(/(?s)\s+/, ' ')
-			def data4		= jsonSlurper.parseText(dataRaw4 ?: "{}")
-			def dataRaw5	= jobPage?.select("script")*.html().find { it.contains("var emailLinkApplyButtonConfig") }?.replaceAll(/(?s).*var emailLinkApplyButtonConfig = ([^;]+);/, '$1')?.replaceAll(/(?s)\s+/, ' ')
-			def data5		= jsonSlurper.parseText(dataRaw5 ?: "{}")
-
 			/*****************/
 			/* Fill Job data */
 			/*****************/
 
 			Job job = new Job()
 			job.source		= sourceID
-			job.idInSource	= extraData.idInSource ?: data?.listing__listing_id ?: data4?.listingData?.id ?: data5?.listingId
+ 			job.idInSource	= extraData.idInSource ?: jobPage.select("#jobId")?.first()?.attr("value")
 			job.url			= pageURL ?: jobPage?.select("link[rel=canonical]")?.first()?.attr("href")
-			job.name		= data?.listing__title?.trim() ?: data4?.listingData?.title
+			job.name		= jobPage.select("h1.brand-font").first().text()
 
-			job.locale		= data5?.locale ?: data3?.locale
-			job.locale		= job.locale ?: (data?.page__language && data?.page__country) ? "${data?.page__language?.toLowerCase()}_${data?.page__country?.toUpperCase()}" : ""
-			job.locale		= job.locale ?: (data4?.language && data4.country) ? "${data4?.language?.toLowerCase()}_${data4?.country?.toUpperCase()}" : ""
-			job.locale		= job.locale ?: data2?.language
-			if (job.locale?.size() == 2) job.locale = "${job.locale}_${this.source.id?.toUpperCase()}"
+			job.locale		= jobPage.select("meta[property=og:locale]")?.first()?.attr("content")
+			job.referenceID = jobPage.select("ul li span.reference")
+					.stream()
+					.filter(it -> it.text().contains("Reference"))
+					?.findFirst()
+					?.get()
+					?.parent()
+					?.text()
+					?.replaceAll("Reference: ", "")
 
-			job.html		= jobPage?.select(".listing-content div[class*=ContentBlock]")?.first()?.html()
+			job.html		= jobPage?.select("div[class^=container job-content]")?.first()?.html()
 			job.text		= DataCleaner.stripHTML(job.html)
-			job.json		= [:] // NOTE: stepstone does not use schemaOrg!!!
-			if (data)	job.json.pageData = data
-			if (data2)	job.json.pageData2 = data2
-			if (data3)	job.json.pageData3 = data3
-			if (data4)	job.json.pageData4 = data4
-			if (data5)	job.json.pageData5 = data5
 
-			try { job.dateCreated = ZonedDateTime.parse(data4?.listingData?.metaData?.onlineDate)?.toLocalDateTime() } catch (e) { /*ignore*/ }
-			try { job.dateCreated = job.dateCreated ?: ZonedDateTime.parse(jobPage?.select("[class*=at-listing__list] time")?.attr("datatime")).toLocalDateTime() } catch (e) { /*ignore*/ }
-			try { job.dateCreated = job.dateCreated ?: LocalDate.parse(jobPage?.select("span[itemprop=datePosted]")?.first()?.text()).atStartOfDay() } catch (e) { /*ignore*/ }
+			def jobScriptJsonText 	= jobPage.select("#jobPostingSchema")?.first()?.childNode(0)?.toString()?.replaceAll("\n", "")?.trim()
 
-			job.position.name			= job.name
-			job.position.workType		= jobPage?.select(".at-listing__list-icons_work-type").text() ?: data4?.listingData?.metaData?.workType
-			job.position.contractType	= jobPage?.select(".at-listing__list-icons_contract-type").text() ?: data4?.listingData?.metaData?.contractType
+			job.json			= [:]
 
-			job.orgTags."${TagType.CATEGORIES}" = (job.orgTags."${TagType.CATEGORIES}" ?: []) + extraData?.category
-			jobPage?.select("#company-hub-block-companyCard section ol > li")*.text()*.split(",")?.flatten()*.trim()?.unique()?.minus("")?.minus(null)?.each {
-				job.orgTags."${TagType.INDUSTRIES}" = (job.orgTags."${TagType.INDUSTRIES}" ?: []) + it
+			// some address parameters can be taken from job script
+			Address address 	= new Address()
+			address.source			= sourceID
+
+			if (jobScriptJsonText) {
+				final JsonSlurper jobJsonSlurper = new JsonSlurper()
+				def jobJsonMap 				= jobJsonSlurper.parseText(jobScriptJsonText)
+				job.json.baseSalary			= jobJsonMap?.baseSalary?.value?.value
+				job.json.unitText			= jobJsonMap?.baseSalary?.value?.unitText
+				job.json.currency			= jobJsonMap?.baseSalary?.currency
+				job.json.title				= jobJsonMap?.title
+				job.json.url				= jobJsonMap?.url
+				job.json.datePosted			= jobJsonMap?.datePosted
+				job.json.industry			= jobJsonMap?.industry
+				job.json.employmentType		= jobJsonMap?.employmentType
+				job.json.hiringOrganization = jobJsonMap?.hiringOrganization?.name
+				job.json.employmentType		= jobJsonMap?.employmentType
+				job.json.jobLocation		= jobJsonMap?.jobLocation?.address?.addressLocality
+
+				// some data are in json like posted date or work type
+				job.dateCreated 			= ZonedDateTime.parse("2020-10-06T08:43+10:00").toLocalDateTime()
+				job.position.workType		= jobJsonMap?.employmentType
+
+				address.country				= jobJsonMap?.jobLocation?.address?.addressCountry
+				address.district			= jobJsonMap?.jobLocation?.address?.addressRegion
+				address.city    			= jobJsonMap?.jobLocation?.address?.addressLocality
+				address.geoPoint			= new GeoPoint(
+						"lat":jobJsonMap?.jobLocation?.geo?.latitude as Double,
+						"lng":jobJsonMap?.jobLocation?.geo?.longitude as Double
+				)
+
 			}
-			jsonSlurper.parseText(jobPage?.select("div[data-block=app-benefitsForListing]")?.first()?.attr("data-initialdata") ?: "{}")?.benefits*.benefitName?.unique()?.each {
-				job.orgTags."${TagType.COMPANY_BENEFITS}" = (job.orgTags."${TagType.COMPANY_BENEFITS}" ?: []) + it
+
+			job.position.name				= job.name
+			job.position.contractType		= jobPage?.select("li.job-type icon")?.first()?.text()
+
+			// career levels can be extracted only if mentioned in job name
+			List<String> careerLevels		= new ArrayList<>()
+			if (job.name.containsIgnoreCase("senior")) {
+				careerLevels.add("Senior")
 			}
+			if (job.name.containsIgnoreCase("middle")) {
+				careerLevels.add("Middle")
+			}
+			if (job.name.containsIgnoreCase("junior")) {
+				careerLevels.add("Junior")
+			}
+			job.position.careerLevel = String.join(", ", careerLevels)
 
 			/**********************/
 			/* Fill Location data */
 			/**********************/
 
-			Location location = new Location()
-			location.source = sourceID
-			try {
-				location.orgAddress.addressLine = data2?.addressText ?: data4?.listingData?.metaData?.location
-				location.orgAddress.addressLine = location.orgAddress.addressLine ?: jobPage.select("footer section address")?.first()?.ownText()
-			} catch (e) {
-			}
-			location.orgAddress.city = data?.listing__location_name ?: data4?.listingData?.metaData?.location
-			try {
-				def addressParts = location.orgAddress.addressLine?.split(",")*.trim()
-				if (addressParts.size() >= 2) {
-					location.orgAddress.country	= location.orgAddress.country	?: addressParts?.getAt(-1)
-					location.orgAddress.city	= location.orgAddress.city		?: addressParts?.getAt(-2)
-					location.orgAddress.street	= location.orgAddress.street	?: addressParts?.getAt(-3)
-				}
-			} catch (e) {}
-			location.orgAddress.countryCode		= location.orgAddress.country ? "" : this.source.id // WARN: does not work for stepstone.de --> includes at and ch
-			// location.orgAddress.country		= location.orgAddress.country		?: this.source.id
-			location.orgAddress.postCode		= data?.listing__zipcode ?: location.orgAddress.city?.find(/\d{4,5}/) // Austria 4, Germany 5 ???
-			location.orgAddress.city			= location.orgAddress.postCode ? location.orgAddress.city?.minus(location.orgAddress.postCode)?.trim() : location.orgAddress.city
-			location.orgAddress.geoPoint.lat	= data2?.latitude as Double
-			location.orgAddress.geoPoint.lng	= data2?.longitude as Double
+			/**********************/
+			/* -------AND-------- */
+			/**********************/
 
 			/*********************/
 			/* Fill Company data */
 			/*********************/
 
+			Location location 			= new Location()
+			location.source 			= sourceID
+
 			Company company = new Company()
 			company.source		= sourceID
-			company.idInSource	= data?.listing__company_id ?: data4?.companyData?.id ?: data5?.companyId
-			company.name		= data2?.addressHead ?: data4?.companyData?.name
-			company.name		= company.name ?: jobPage?.select(".listing__top-info .listing__company-name")?.text() ?: data4?.companyData?.name ?: data2?.addressHead
-			def companyLink = data4?.companyData?.companyPageURL?.replaceAll(/(\/cmp\/..\/[^\/]+).*/, '$1')
-			companyLink = companyLink ?: jobPage?.select("a.at-header-company-logo[href*=/cmp/]")?.first()?.absUrl("href")?.replaceAll(/(\/cmp\/..\/[^\/]+).*/, '$1')
-			companyLink = companyLink ?: jobPage?.select("a.at-company-hub-link")?.first()?.absUrl("href")?.replaceAll(/\/?\?.*/, '')
-			companyLink = companyLink ?: jobPage?.select(".listing__top-info .listing__company-name a")?.first()?.absUrl("href")?.replaceAll(/\/?\?.*/, '')
-			// NOTE: "jobs.html" (de) must be removed but "work.html" (be) should not be removed !?
-			companyLink = companyLink?.replaceAll(/\/jobs.html$/, '')
-			company.urls		= [("$sourceID" as String): companyLink]
-			company.ids			= [("$sourceID" as String): company.idInSource]
-			if (company.name?.find(/(?i)Jobs via StepStone/)) return false // job is from another Stepstone portal
-			if (company.idInSource == "anonymous") return false // job is from anonymous company
+
+			def companyScriptJsonText 	= jobPage.select("script[language=JavaScript]")?.first()?.childNode(0)?.toString()?.replaceAll("var analytics = ", "")?.replaceAll("\n", "")?.trim()
+			job.json			= [:]
+			if (companyScriptJsonText) {
+				final JsonSlurper companyJsonSlurper = new JsonSlurper()
+				def companyJsonMap 		= companyJsonSlurper.parseText(companyScriptJsonText)
+				location.description	= companyJsonMap?.City
+				location.name			= companyJsonMap?.JobTitle
+				location.dateCreated	= job.dateCreated
+				location.idInSource		= companyJsonMap?.JobId as String
+
+				address.companyName 	= companyJsonMap?.CompanyName
+				address.addressLine 	= companyJsonMap?.City
+				address.postCode	 	= companyJsonMap?.JobPostcode
+				address.county 			= companyJsonMap?.CompanyName
+
+				location.orgAddress		= address
+				location.contacts		= [new Contact("name": jobPage.select("ul li span.reference")
+												.stream()
+												.filter(it -> it.text().contains("Contact"))
+												?.findFirst()
+												?.get()
+												?.parent()
+												?.text()
+												?.replaceAll("Contact: ", "")
+										)]
+
+				company.idInSource	= companyJsonMap?.CompanyId
+				company.name		= companyJsonMap?.CompanyName
+				company.url 		= jobPage.select("#companyJobsLink").first()?.absUrl("href")
+				company.urls		= [("$sourceID" as String): company.url]
+				company.ids			= [("$sourceID" as String): company.idInSource]
+
+				job.orgTags."${TagType.COMPANY_TYPES}"		= [companyJsonMap?.SubDisciplines]
+				job.orgTags."${TagType.INDUSTRIES}"			= [companyJsonMap?.Disciplines]
+			}
+
+			job.orgTags."${TagType.CAREER_LEVELS}"		= [job.position.careerLevel]
+			job.orgTags."${TagType.CATEGORIES}"			= [job.orgCompany.name]
+			job.orgTags."${TagType.CONTRACT_TYPES}"		= [job.position.contractType]
+			job.orgTags."${TagType.JOBNAMES}"			= [job.name]
+			job.orgTags."${TagType.LANGUAGES}"			= ["en"]
+			job.orgTags."${TagType.WORK_TYPES}"			= [job.position.workType]
 
 			/*******************/
 			/* Store page data */
